4f7e55524a69fe54e9ce987b4d0fdf1c3dda7ebb
caisongyang <caisongyang@wei-honour.com>
2019-11-19 17:40:52 +0800

    1.修改文件

--- src/com/common/filter/LoginFilter.java
+++ src/com/common/filter/LoginFilter.java
@@ -1,12 +1,9 @@
 package common.filter;
 
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
-
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * @author:Hongyuan.Niu
@@ -21,42 +20,29 @@ import java.util.Map;
  * @DATE 2018年8月27日 下午1:50:51
  */
 public class LoginFilter extends OncePerRequestFilter {
-    private String LOGIN_VIEW = "/module/common/login.html";
-    static String suffix[] = { ".png", ".css", ".js" };
+    private String LOGIN_VIEW = "/html/login.html";
+    static String suffix[] = { ".png", ".css", ".js",".gif" };
 
-    static Map<String, String> mobileUrlMap;
-    static{
-        mobileUrlMap = new HashMap<String,String>();
-    }
-
-    private static Logger logger = LogManager.getLogger(LoginFilter.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        if(mobileUrlMap.get(request.getRequestURI().replace("//", "/"))!=null&&!"".equals(mobileUrlMap.get(request.getRequestURI().replace("//", "/")))){
+
+        System.out.println("request getContextPath = "+request.getContextPath());
+        System.out.println("request getRequestURI = "+request.getRequestURI());
+        if("/HKHome/html/login.html".equals(request.getRequestURI())){
             filterChain.doFilter(request, response);
         }else{
-            // 如果没有登录.
-            String requestURI = "";
-            if("/".equals(request.getRequestURI()))
-                requestURI = "/";
-            else
-                requestURI = request.getRequestURI().substring(request.getRequestURI().indexOf("/", 1),
-                        request.getRequestURI().length());
-            logger.info("requestURI:"+requestURI);
-            String contextPath = "";
-            if(contextPath!=null&&!"".equals(contextPath.trim()))
-                contextPath = request.getContextPath();
-            else
-                contextPath = "/AiEO";
-            if (request.getSession().getAttribute("CURRENT_USER") == null && !this.notFilter(requestURI) && !requestURI.contains("login")) {
-                response.sendRedirect(contextPath+LOGIN_VIEW);
+            if (request.getSession().getAttribute("CURRENT_USER")==null&&notFilter(request.getRequestURI())){
+                response.sendRedirect(request.getContextPath()+LOGIN_VIEW);
                 return;
+            }else{
+                filterChain.doFilter(request, response);
             }
-
-            filterChain.doFilter(request, response);
         }
+
     }
+
+
 
     private boolean notFilter(String url) {
         if (StringUtils.isNotEmpty(url) && url.indexOf(".") > -1) {
