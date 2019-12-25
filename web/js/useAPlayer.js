c83330e9a984260d35465246c440595845621af9
caisongyang <caisongyang@wei-honour.com>
2019-12-25 17:35:30 +0800

    1.修改文件

--- web/js/useAPlayer.js
+++ web/js/useAPlayer.js
@@ -2,13 +2,23 @@ $(function(){
     console.log("use - APlayer");
     const ap = new APlayer({
         container: document.getElementById('aplayer'),
-        fixed: true,
+        fixed:false,
         audio: [{
             name: 'name',
             artist: 'artist',
             url: '/HKHome/APlayermp3/1/1.mp3',
-            cover: '/HKHome/APlayermp3/1/1.jpg'
-        }]
+            cover: '/HKHome/APlayermp3/1/1.jpg',
+            theme: '#df0f0f'
+        },
+            {
+                name: 'name',
+                artist: 'artist',
+                url: '/HKHome/APlayermp3/1/1.mp3',
+                cover: '/HKHome/APlayermp3/1/1.jpg',
+                theme: '#050505'
+            }
+
+             ]
     });
 
 });
